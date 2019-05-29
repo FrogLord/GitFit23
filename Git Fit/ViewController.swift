f5ecb73c1b1b039b9e3ba04fc44afc244368803c
Kurt Galicha <frogpheus@gmail.com>
2019-05-29 13:37:39 -0600

    Initial Commit

---
+++ Git Fit/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Git Fit
+//
+//  Created by Kurt Galicha on 2019-05-29.
+//  Copyright © 2019 The Computer bouz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+

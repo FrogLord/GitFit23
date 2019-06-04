1393b611f2e5afccd514e2b62e2512cca662afe6
austinw6 <austinw6@learn.cssd.ab.ca>
2019-06-04 13:50:44 -0600

    selector added

--- Git Fit/ViewController.swift
+++ Git Fit/ViewController.swift
@@ -13,6 +13,11 @@ class ViewController: UIViewController {
     var strength = false
     var endurance = false
     var average = true
+    var buttonDisable1 : Int = 0
+    var buttonDisable2 : Int = 0
+    var buttonDisable3 : Int = 0
+    var buttonDisable4 : Int = 0
+    var buttonDisable5 : Int = 0
     
     var picker1 = ["Upper Body", "Legs", "Core", "Arms", "Cardio"]
     
@@ -30,37 +35,72 @@ class ViewController: UIViewController {
     
   
     
-    @IBAction func workout1() {
+
         
-    }
+    
     @IBOutlet weak var createWorkoutButton: UIButton!
     @IBOutlet weak var strengthButton: UIButton!
     @IBOutlet weak var enduranceButton: UIButton!
     
     
-    @IBOutlet weak var selectorWheel: UIPickerView!
-    
-    
-    @IBAction func enduranceCreate() {
-        
-    }
-    
-    @IBAction func createRecipe() {
-        
-    }
-    
-    
     override func viewDidLoad() {
         super.viewDidLoad()
-        selectorWheel.isHidden = true
+        
         
     }
     
-    @IBAction func selector() {
-        
+    @IBAction func button1pressed(sender: UIButton) {
+        if sender.tag == 1 && buttonDisable1 == 0 {
+        button1.backgroundColor = UIColor.red
+            buttonDisable1 = 1
+        } else if sender.tag == 1 && buttonDisable1 == 1 {
+            button1.backgroundColor = UIColor.green
+            buttonDisable1 = 0
+        }
+    }
+    
+    @IBAction func button2pressed(sender: UIButton) {
+        if sender.tag == 2 && buttonDisable2 == 0 {
+            button2.backgroundColor = UIColor.red
+            buttonDisable2 = 1
+        } else if sender.tag == 2 && buttonDisable2 == 1 {
+            button2.backgroundColor = UIColor.green
+            buttonDisable2 = 0
+        }
+    }
+    
+    
+    @IBAction func button3pressed(sender: UIButton) {
+        if sender.tag == 3 && buttonDisable3 == 0 {
+            button3.backgroundColor = UIColor.red
+            buttonDisable3 = 1
+        } else if sender.tag == 3 && buttonDisable3 == 1 {
+            button3.backgroundColor = UIColor.green
+            buttonDisable3 = 0
+        }
+    }
+    
+    @IBAction func button4pressed(sender: UIButton) {
+        if sender.tag == 4 && buttonDisable4 == 0 {
+            button4.backgroundColor = UIColor.red
+            buttonDisable4 = 1
+        } else if sender.tag == 4 && buttonDisable4 == 1 {
+            button4.backgroundColor = UIColor.green
+            buttonDisable4 = 0
+        }
+    }
+    
+    @IBAction func button5pressed(sender: UIButton) {
+        if sender.tag == 5 && buttonDisable5 == 0 {
+            button5.backgroundColor = UIColor.red
+            buttonDisable5 = 1
+        } else if sender.tag == 5 && buttonDisable5 == 1 {
+            button5.backgroundColor = UIColor.green
+            buttonDisable5 = 0
+        }
     }
 
-   
-
 }
+
+
 

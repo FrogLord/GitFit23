fa326bc2af657e7f9cbe7c4313b3eb70c86012fb
Kurt Galicha <frogpheus@gmail.com>
2019-06-07 11:56:08 -0600

    Stopwatch Kind of Working

--- Git Fit/ThirdViewController.swift
+++ Git Fit/ThirdViewController.swift
@@ -9,10 +9,70 @@
 import UIKit
 
 class ThirdViewController: UIViewController {
-
+    
+    var stopWatchTimer = Timer()
+    var currentTime = 0
+    
+    
+    //IBOutlets
+    
+    @IBOutlet weak var minuteLabel: UITextField!
+    @IBOutlet weak var secondLabel: UITextField!
+    @IBOutlet weak var startButton: UIButton!
+    @IBOutlet weak var stopButton: UIButton!
+    @IBOutlet weak var pauseButton: UIButton!
+    
+    //IBActions
+    @IBAction func startButtonPressed(_ sender: Any) {
+        startButton.isHidden = true
+        pauseButton.isHidden = false
+        stopButton.isEnabled = true
+        
+        Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(ThirdViewController.updateTime), userInfo: nil, repeats: true)
+    }
+    @IBAction func stopButtonPressed(_ sender: Any) {
+        stopWatchTimer.invalidate()
+        
+        startButton.isHidden = false
+        pauseButton.isHidden = true
+        stopButton.isEnabled = false
+        
+        currentTime = 0
+        
+        secondLabel.text = "0"
+        minuteLabel.text = "0"
+        
+    }
+    @IBAction func pauseButtonPressed(_ sender: Any) {
+        stopWatchTimer.invalidate()
+        
+        startButton.isHidden = false
+        pauseButton.isHidden = true
+        
+    }
+    @objc func updateTime() {
+        currentTime += 1
+        minuteLabel.text = "\(currentTime / 60)"
+        secondLabel.text = "\(currentTime % 60)"
+        
+        if currentTime == 3600 {
+            currentTime = 0
+            stopWatchTimer.invalidate()
+        }
+        else {
+            print("Time Failed")
+        }
+    }
+    
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        pauseButton.isHidden = true
+        stopButton.isEnabled = false
+        
         // Do any additional setup after loading the view.
     }
     
